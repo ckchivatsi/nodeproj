c307b2a2dfac86816f8595c2ce5ffc811070c1c3
ckchivatsi <kelvinchivatsi@hotmail.com>
2016-12-29 12:49:09 +0300

    Basic Concepts

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Hello World, am ready to learn NodeJS")
+//variables
+var person = {
+    name : 'Chai',
+    age : 23
+};
+console.log(person);
+
+//functions
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log(addNumbers(7, 3));
+
+function worthless() {
+    //function without a return value
+}
+console.log(worthless());
+
+//anonymous function
+var msg = function () {
+    console.log("Hello World")
+}
+msg();
+
+setTimeout(msg, 5000);

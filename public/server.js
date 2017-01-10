d42b3e9f58ce5f40f12d9b4a78f7cf9830a8ea90
ckchivatsi <kelvinchivatsi@hotmail.com>
2017-01-10 22:42:34 +0300

    connect module

--- public/server.js
+++ public/server.js
@@ -1,22 +1,18 @@
-var http = require('http');
-var fs = require('fs');
+var connect = require ('connect');
+var http = require ('http');
 
-//404 response
-function send404Response(response) {
-    response.writeHead(404, {"Content-Type": "text/plain"});
-    response.write("Error 404 Page not found!");
-    response.end();
+var app = connect();
+
+function profile(request, response) {
+    console.log("User requested profile page.")
 }
 
-//Handle a user request
-function onRequest(request, response) {
-    if (request.method == 'GET' && request.url == '/'){
-        response.writeHead(200, {"Content-Type": "text/html"});
-        fs.createReadStream("./index.html").pipe(response);
-    }else {
-        send404Response(response);
-    }
+function forum(request, response) {
+    console.log("User requested forum page.")
 }
 
-http.createServer(onRequest).listen(8888);
-console.log("server is now running...");
+app.use('/profile', profile);
+app.use('/forum', forum);
+
+http.createServer(app).listen(8888);
+console.log("server is running...");
